608bb98792b8117fcc93905e84c10c2f23ea8901
Luiz Carlos <luizcdev@outlook.com>
2024-12-13 05:07:25 -0300

    Finalizando todo o projeto com a parte de JS

---
+++ index.js
@@ -0,0 +1,20 @@
+const botao = document.querySelector(".btn-plataforma");
+
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {
+
+const botaoEstaAberto = elementoPlataformas.classList.
+contains("ativo");
+ console.log(botaoEstaAberto);
+
+ if(botaoEstaAberto){ 
+   elementoPlataformas.classList.remove("ativo");
+}else{
+  
+   elementoPlataformas.classList.add("ativo");
+}
+});
+
+
+
